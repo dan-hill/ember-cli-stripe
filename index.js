acad152483818fc053d0e141d0cd2be2e06fc119
Vlad Vanca <vanca.vlad@gmail.com>
2015-10-29 14:14:48 +0200

    Upgrade to ember-cli 1.13.8

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+/* jshint node: true */
 'use strict';
 
 module.exports = {
   name: 'ember-cli-stripe',
-  
+
   contentFor: function(type) {
     if (type === 'body') {
       return '<script src="https://checkout.stripe.com/checkout.js"></script>';
